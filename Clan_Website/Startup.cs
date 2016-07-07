fb051adda03b4a44e609083087c5bab52f68a71e
TheBackN9NE <lancesnell7389@gmail.com>
2016-07-07 09:36:35 -0500

    Added default website

---
+++ Clan_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clan_Website.Startup))]
+namespace Clan_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
